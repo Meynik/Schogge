549b6438788b413e046698aa90800f79616eb8a6
Meynik <momenik@hotmail.de>
2026-07-05 11:22:53 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "schogge-cache-v12";
+const CACHE_NAME = "schogge-cache-v13";
 const ASSETS = [
   "./",
   "./index.html",
--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.SCHOGGE_SUPABASE_CONFIG = {
-  url: "https://kbcxilczsqfmeunpprqy.supabase.co/rest/v1/",
+  url: "https://kbcxilczsqfmeunpprqy.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtiY3hpbGN6c3FmbWV1bnBwcnF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMyMTE0MjYsImV4cCI6MjA5ODc4NzQyNn0.6HHml79lEhIcPMhJQbVGtujSsaERFWzrDYMCAYE3Gk8",
 };
